2aa3a5cf28f446399c980681bf71e65341b59f44
Helder Valente <helderochavalentee@gmail.com>
2025-02-14 14:49:54 +0000

    ola

--- src/store/Tasks.store.tsx
+++ src/store/Tasks.store.tsx
@@ -7,36 +7,6 @@ import {
 } from "@reduxjs/toolkit";
 import { Task } from "../interfaces";
 
-const defaultTasks: Task[] = [
-  {
-    title: "Task 1",
-    important: false,
-    description: "This is the description for this task",
-    date: "2023-04-12",
-    dir: "Main",
-    completed: true,
-    id: "t1",
-  },
-  {
-    title: "Task 2",
-    important: true,
-    description: "This is the description for this task",
-    date: "2023-05-15",
-    dir: "Main",
-    completed: true,
-    id: "t2",
-  },
-  {
-    title: "Task 3",
-    important: false,
-    description: "This is the description for this task",
-    date: "2023-08-21",
-    dir: "Main",
-    completed: false,
-    id: "t3",
-  },
-];
-
 const getSavedDirectories = (): string[] => {
   let dirList: string[] = [];
   if (localStorage.getItem("directories")) {
@@ -70,7 +40,7 @@ const initialState: {
 } = {
   tasks: localStorage.getItem("tasks")
     ? JSON.parse(localStorage.getItem("tasks")!)
-    : defaultTasks,
+    : [], // Array vazio se não houver tarefas no localStorage
   directories: getSavedDirectories(),
 };
 
@@ -175,7 +145,6 @@ export const tasksMiddleware =
     }
 
     if (tasksActions.removeTask.match(action)) {
-      console.log(JSON.parse(localStorage.getItem("tasks")!));
       if (localStorage.getItem("tasks")) {
         const localStorageTasks = JSON.parse(localStorage.getItem("tasks")!);
         if (localStorageTasks.length === 0) {
